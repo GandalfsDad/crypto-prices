f69da9de1eef242870f8fd78a8127456e766fc44
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-08-10 14:56:59 +1000

    Add basic Binance Query

--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,19 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 )
 
 func main() {
-	fmt.Println(os.Getenv("CMC_KEY"))
+	pair := os.Args[1]
+	s, err := http.Get("https://api.binance.com/api/v3/avgPrice?symbol=" + pair)
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	io.Copy(os.Stdout, s.Body)
 }
