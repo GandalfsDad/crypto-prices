52196688d04912489ed058f119b8859bc3be0795
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-08-10 18:08:27 +1000

    Edit main script

--- cmd/main.go
+++ cmd/main.go
@@ -1,20 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"net/http"
 	"os"
+
+	"github.com/GandalfsDad/crypto-prices/pkg/pricing"
 )
 
 func main() {
 	pair := os.Args[1]
-	s, err := http.Get("https://api.binance.com/api/v3/avgPrice?symbol=" + pair)
 
-	if err != nil {
-		fmt.Println("Error:", err)
-		os.Exit(1)
-	}
+	pg := pricing.BnbAvgPriceGetter{}
 
-	io.Copy(os.Stdout, s.Body)
+	pricing.PrintPrice(pg, pair)
 }
